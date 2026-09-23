4b8e547d091977ae282661017c0f88d244cbfc81
Shawn Jr <shawnmrobertsjr@gmail.com>
2026-09-22 21:49:59 -0700

    Update yoga.ts

--- src/content/yoga.ts
+++ src/content/yoga.ts
@@ -9,6 +9,6 @@ export const yogaContent = {
   about: {
     title: 'Equanimity of mind is Yoga',
     photo: '/assets/yoga-about.jpg',
-    bio: "Shawn Jr offers classes rooted in breath, self-awareness, and compassion. Trained through Folk's 200 & 300-hour program and now a 500 RYT, Shawn teaches with the aim of helping students feel more connected, calm, and resilient. He honors the traditional foundations of yoga while staying attuned to the needs of today's students.",
+    bio: "Shawn Jr offers classes rooted in breath, self-awareness, and equanimity. Trained through Folk's 200 & 300-hour program and now a 500 RYT, Shawn teaches with the aim of helping students feel more connected, calm, and resilient. He honors the traditional foundations of yoga while staying attuned to the needs of today's students.",
   },
 }
